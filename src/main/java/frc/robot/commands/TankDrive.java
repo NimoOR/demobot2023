8baaa61ee104058c65e47f1a2e04084a7dfb1fb0
Nimo <nimoohayonr@gmail.com>
2023-01-11 19:04:03 -0600

    specific stuff

--- src/main/java/frc/robot/commands/TankDrive.java
+++ src/main/java/frc/robot/commands/TankDrive.java
@@ -4,23 +4,22 @@
 
 package frc.robot.commands;
 
-import frc.robot.subsystems.ExampleSubsystem;
+
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.Drive;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 /** An example command that uses an example subsystem. */
-public class ExampleCommand extends CommandBase {
+public class TankDrive extends CommandBase {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final ExampleSubsystem m_subsystem;
-
-  /**
-   * Creates a new ExampleCommand.
-   *
-   * @param subsystem The subsystem used by this command.
-   */
-  public ExampleCommand(ExampleSubsystem subsystem) {
-    m_subsystem = subsystem;
+  
+  private final Drive drive;
+  private final RobotContainer robotContainer;
+  public TankDrive(RobotContainer container, Drive subsystem) {
+    drive = subsystem;
+    robotContainer = container;
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(subsystem);
+    addRequirements(drive);
   }
 
   // Called when the command is initially scheduled.
@@ -29,7 +28,11 @@ public class ExampleCommand extends CommandBase {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+
+    drive.setPower(robotContainer.getDriveRightJoystick(), robotContainer.getDriverLeftJoystick());
+
+  }
 
   // Called once the command ends or is interrupted.
   @Override
--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -3,45 +3,39 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems;
-
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.RobotContainer;
 
-public class ExampleSubsystem extends SubsystemBase {
-  /** Creates a new ExampleSubsystem. */
-  public ExampleSubsystem() {}
+public class Drive extends SubsystemBase {
+  private static Joystick joystick;
 
-  /**
-   * Example command factory method.
-   *
-   * @return a command
-   */
-  public CommandBase exampleMethodCommand() {
-    // Inline construction of command goes here.
-    // Subsystem::RunOnce implicitly requires `this` subsystem.
-    return runOnce(
-        () -> {
-          /* one-time action goes here */
-        });
+  private final static Spark leftmotors1 = new Spark(1);
+  private final static Spark leftmotors2 = new Spark(2);
+  private final static Spark rightmotors1 = new Spark(3);
+  private final static Spark rightmotors2 = new Spark(4);
+  public Drive(Joystick stick) {
+    
+    joystick = stick;
+  
   }
+  
+  public void setPower(double leftPower, double rightPower){
+    leftmotors1.set(leftPower);
+    leftmotors2.set(leftPower);
 
-  /**
-   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
-   *
-   * @return value of some boolean subsystem state, such as a digital sensor.
-   */
-  public boolean exampleCondition() {
-    // Query some boolean state, such as a digital sensor.
-    return false;
+    rightmotors1.set(rightPower);
+    rightmotors2.set(rightPower);
   }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    setPower(joystick.getRawAxis(5),joystick.getRawAxis(1));
   }
 
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
-  }
 }
